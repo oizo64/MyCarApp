2f442e3d40b71e89a4cd5b3cbfddbc7ff91482f1
Piotr <oizo64@gmail.com>
2026-04-05 23:51:58 +0200

    Czyszczenie

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.example.mycarapp"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,4 +22,3 @@ dependencyResolutionManagement {
 rootProject.name = "MyCarApp"
 include(":mobile")
 include(":automotive")
-
